09a6f09d402373014dda48ee7e9ccfb8ad34e705
Spike Grobstein <me@spike.cx>
2012-10-17 20:48:54 -0400

    removed some depricated Rspec stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,6 @@ $: << File.dirname(__FILE__) + '/../lib'
 require 'capistrano'
 require 'capistrano-mailgun'
 
-Rspec.configure do |config|
-  # config
-end
-
 def fixture_path(filename)
   File.join( File.dirname(__FILE__), 'fixtures', filename )
 end
