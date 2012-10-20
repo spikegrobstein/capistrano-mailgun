4d5a2a91a7afbe1321d4d65b8ce4aea0dc3d71ae
Spike Grobstein <me@spike.cx>
2012-10-19 21:59:24 -0400

    WIP on log_output

--- lib/capistrano-mailgun.rb
+++ lib/capistrano-mailgun.rb
@@ -150,6 +150,17 @@ module Capistrano
       "https://api:#{ mailgun_api_key }@api.mailgun.net/v2/#{ mailgun_domain }/messages"
     end
 
+
+    # git log between +first_ref+ to +last_ref+
+    def log_output(first_ref, last_ref)
+      log_output = run_locally("git log --oneline #{ first_ref }..#{ last_ref }")
+
+      log_output = log_output.split("\n").map do |line|
+        fields = line.match /^(\w{7,})\s+(.+?)$/
+        [ fields[0], fields[1] ]
+      end
+    end
+
   end
 end
 
